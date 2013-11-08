9944e0e0a5dc47894f02a656d35be81e417fe01f
Maxim Mitroshin <rocco66max@gmail.com>
2013-11-08 16:07:50 +0400

    Add plural translate filter

--- src/filter.js
+++ src/filter.js
@@ -3,3 +3,19 @@ angular.module('gettext').filter('translate', function (gettextCatalog, $interpo
         return gettextCatalog.getString(input);
     };
 });
+
+angular.module('gettext').filter('translateN', function (gettextCatalog, $interpolate, $parse) {
+    return function (input, n, translatePlural) {
+        // Validate filter parameters
+        var nIsNumber = typeof(n) === "number",
+            nIsStringNumber = typeof(parseInt(n, 10)) === "number";
+        if (!nIsNumber && !nIsStringNumber) {
+            throw new Error("First parameter of translateN must be a number, or a string with number, not " + n + ".");
+        }
+        if (typeof(translatePlural) !== "string") {
+            throw new Error("Second parameter of translateN must be a string with plural form, not " + n + ".");
+        }
+
+        return gettextCatalog.getPlural(n, input, translatePlural);
+    };
+});
